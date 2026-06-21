e316dbcb9b8112890341d7554ac3f121f250a294
Steve Koks <stevekoksg@gmail.com>
2026-06-21 20:32:49 +0300

    classes

--- classes/item.py
+++ classes/item.py
@@ -1,12 +1,13 @@
-from abc import ABC, abstractmethod
-from typing import Optional
+from abc import ABC
 
 import preferences
+
+from classes.character import Character
 
 
 class Item(ABC):
     def __init__(
-        self, id: int, name: str, weight: float, value: int, max_stack: int = 1
+        self, id: int, name: str, weight: float, value: int, max_stack: int
     ) -> None:
         self.id = id
         self.name = name
@@ -15,12 +16,35 @@ class Item(ABC):
         self.quantity = 1
         self.max_stack = max_stack
 
-    @abstractmethod
-    def use(self, user, target=None):
-        pass
-
     def get_info(self):
         if preferences.DEBUG:
             return (
-                f"({self.id}){self.name} (вес: {self.weight}, ценность: {self.value})"
+                f"[{self.id}]{self.name} (вес: {self.weight}, ценность: {self.value})"
             )
+        return f"{self.name} (вес: {self.weight}, ценность: {self.value})"
+
+
+class SwordItem(Item):
+    def __init__(
+        self,
+        id: int,
+        name: str,
+        weight: float,
+        value: int,
+        max_stack: int,
+        damage: int,
+        durability: int,
+        current_durability: int,
+    ) -> None:
+        super().__init__(id, name, weight, value, max_stack)
+        self.damage = damage
+        self.durability = durability
+        self.current_durability = current_durability
+
+    def use(self, target: Character):
+        if self.current_durability <= 0:
+            return f"{self.name} сломан!"
+        target.take_damage(self.damage)
+        item_damage = target.armor // 2
+        self.durability -= item_damage
+        target.set_armor(target.armor - self.damage // 2)
--- classes/monster.py
+++ classes/monster.py
@@ -1,45 +1,8 @@
-class Monster:
-    def __init__(self, health: int, damage: int, armor:int, default_health: int = None, name: str = "Monster",):
-        self.health = health
-        
-        if default_health:
-            self.default_health = default_health
-        else:
-            self.default_health = health
-        self.damage = damage
-        self.armor = armor
-        self.name = name
-        
-    def take_damage(self, value: int):
-        self.health -= value - self.armor
-    
-    def heal(self):
-        self.health = self.default_health
-    
-    #СЕТТЕРЫ
-    def set_health(self, value: int):
-        self.health = value
-        
-    def set_damage(self, value: int):
-        self.damage = value
-    
-    def set_armor(self, value: int):
-        self.armor = value
-    
-    def set_name(self, value: str):
-        self.name = value
-        
-    
-    #ГЕТТЕРЫ
-    def get_health(self) -> int:
-        return self.health
-    
-    def get_damage(self) -> int:
-        return self.damage
-    
-    def get_armor(self) -> int:
-        return self.armor
-    
-    def get_name(self) -> str:
-        return self.name
-        
+from classes.character import Character
+
+
+class Monster(Character):
+    def __init__(
+        self, name: str, health: int, damage: int, armor: int, default_health: int = 0
+    ):
+        super().__init__(name, health, damage, armor, default_health)
